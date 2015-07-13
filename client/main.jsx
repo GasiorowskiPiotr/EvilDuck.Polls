619cbb3fa0e47588d49edad442222d178205adad
unknown <B4P@LW5B4PCEM.dsone.3ds.com>
2015-07-13 13:48:45 +0200

    Initial commit.

---
+++ client/main.jsx
@@ -0,0 +1,8 @@
+import { Router, Route, Link } from 'react-router';
+import React from 'react';
+
+export default class Main extends React.Component {
+  render() {
+    
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var merge = require('webpack-merge');
+var webpack = require('webpack');
+
+var TARGET = process.env.TARGET || 'dev';
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+  entry: [path.resolve(ROOT_PATH, 'client/main.jsx')],
+  output: {
+    path: path.resolve(ROOT_PATH, 'public/javascripts'),
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+    },
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel?stage=1',
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ],
+  }
+};
+
+// we'll extend these later and use merge then
+if(TARGET === 'build') {
+  module.exports = merge(common, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
+}
+
+if(TARGET === 'dev') {
+  module.exports = common;
+}
